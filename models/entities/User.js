5b83b82f0c8254b05fae88abd55fd3ef4f3037df
koc9n <kos.mironchik@gmail.com>
2016-07-13 17:39:44 +0700

    updated working with models

---
+++ models/entities/User.js
@@ -0,0 +1,14 @@
+/**
+ * Created by koc9n on 7/13/2016 AD.
+ */
+var User = {
+    attributes: {
+        username: {type: 'string', unique: true},
+        profileUrl: {type: 'link'},
+        email: {type: 'email', unique: true},
+        passports: {collection: 'Passport', via: 'user'},
+        photos: {type: 'array'}
+    }
+};
+
+module.exports = User;
